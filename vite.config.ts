8a038749194f7b6b881a12adc395627642ca66c7
aqui <aquilesffoliveira@gmail.com>
2025-03-03 15:51:44 -0300

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,13 @@ export default defineConfig({
       '$lib/components': path.resolve('./src/lib/components'),
       '$lib/utils': path.resolve('./src/lib/utils')
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        notFound: path.resolve(__dirname, 'static/404.html')
+      }
+    }
   }
 })
